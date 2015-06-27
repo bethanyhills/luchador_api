b3b801cbdd9cd40829e80c2ac5e54213c37e52b3
Bethany Nagel <beth0851@gmail.com>
2015-06-27 12:35:07 -0700

    Added GET by luchador fact id

--- api.py
+++ api.py
@@ -1,14 +1,25 @@
 from flask import Flask
-from flask_restful import Resource, API
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+facts = {
+    '1': "Luchadors wrestle because they wish to be hugged",
+    '2': "Luchadors wear masks because they are too handsome"}
+
+class LuchadorFacts(Resource):
+    def get(self, fact_id):
+            return {fact_id: facts[fact_id]}
+
+
+
+# class HelloWorld(Resource):
+#     def get(self):
+#         return {'hello': 'world'}
+
+api.add_resource(LuchadorFacts, '/<string:fact_id>')
 
 if __name__ == '__main__':
     app.run(debug = True)
