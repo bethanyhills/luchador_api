3809ce8e142b55043681db4d97861c93b047e4e4
Bethany Nagel <beth0851@gmail.com>
2015-07-22 16:07:01 -0500

    Increase character limit for fact

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def unauthorized():
 class Fact(db.Model):
     __tablename__ = 'facts'
     id = db.Column(db.Integer, primary_key=True)
-    fact = db.Column(db.String(120), unique=True)
+    fact = db.Column(db.String(500), unique=True)
 
     def __init__(self, fact):
         self.fact = fact
