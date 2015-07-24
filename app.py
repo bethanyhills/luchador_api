ddd43b4adf0fd95a431d083e5ce56326e5421c42
Bethany Nagel <beth0851@gmail.com>
2015-07-23 20:19:16 -0400

    Removed character limit on fact

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ auth = HTTPBasicAuth()
 class Fact(db.Model):
     __tablename__ = 'facts'
     id = db.Column(db.Integer, primary_key=True)
-    fact = db.Column(db.String(500), unique=True)
+    fact = db.Column(db.String, unique=True)
 
     def __init__(self, fact):
         self.fact = fact
